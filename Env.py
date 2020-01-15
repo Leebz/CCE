b07b7d348ab2ba132f9697d615fee63b60e3adbf
lbz <unitity0086@gmail.com>
2020-01-15 19:02:08 +0800

    678

--- Env.py
+++ Env.py
@@ -2,5 +2,10 @@ import numpy as np
 
 
 class Env(object):
-    def __init__(self):
+    def __init__(self,
+                 seed,
+                 max_episode_step,
+                 task_size_low,
+                 task_size_high,
+                 task_computation_demand,):
         pass
